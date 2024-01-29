cdac436d856c081d76625055a762226dfad593be
Mohan AC <acmohan91@gmail.com>
2024-01-29 22:39:52 +0530

    Create EssentialFeed Framework with FeeItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mohan AC on 29/01/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
